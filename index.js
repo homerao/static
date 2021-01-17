d9b929954b84140cafe85a364d84a13e5eb84375
Homero <homerao@gmail.com>
2021-01-17 15:56:04 -0300

    configurando app de arquivos estáticos

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+
+const app = express()
+
+app.use(express.static('public'))
+
+
+app.listen(process.env.PORT, function (){
+    console.log('Servidor estático iniciado')
+})
